eb15ae38b428b5e9c0e1cbc0f117350ae1b31837
Sagar Sakharam Parkar <160031785+Sagarpar10@users.noreply.github.com>
2024-05-22 17:09:21 +0530

    Add files via upload

---
+++ user.model.ts
@@ -0,0 +1,14 @@
+export class Usermodel {
+    id?: number = 0;
+    firstname: string = '';
+    lastname: string = '';
+    email?: string;
+    age: string = '';
+    contact?: string;
+    state: string = '';
+    country: string = '';
+    address: string = '';
+    hobby : string = '';
+    profile: string = '';
+  }
+  
